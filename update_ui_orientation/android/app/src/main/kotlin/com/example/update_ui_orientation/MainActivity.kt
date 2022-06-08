af80a30bde25c347f0bf9037b7ed9dc391fcd1a7
wusung <twkoci@gmail.com>
2022-06-08 22:51:10 +0800

    update the ui based on orientation

---
+++ update_ui_orientation/android/app/src/main/kotlin/com/example/update_ui_orientation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.update_ui_orientation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ update_ui_orientation/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Orientation Demo';
+
+    return const MaterialApp(
+      title: appTitle,
+      home: OrientationList(title: appTitle),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  const OrientationList({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(100, (index) {
+              return Center(
+                  child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline1,
+              ));
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
