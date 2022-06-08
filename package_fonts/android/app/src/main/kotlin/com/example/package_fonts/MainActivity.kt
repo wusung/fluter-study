a8749fb38bb0a67d72361335facd4580de6c66c7
wusung <twkoci@gmail.com>
2022-06-08 22:30:01 +0800

    export fonts from a package

---
+++ package_fonts/android/app/src/main/kotlin/com/example/package_fonts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.package_fonts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ package_fonts/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Package Fonts',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Package Fonts'),),
+      body: const Center(
+        child: Text(
+          'Using the Raleway font from the awesome_package',
+          style: TextStyle(
+            fontFamily: 'Raleway',
+            package: 'awesome_package',
+          ),
+        )
+      ),
+    );
+  }
+}
