44e0c0e96bc945462cc5b8b3d3bf121b4fa9ed11
wusung <twkoci@gmail.com>
2022-06-09 13:41:15 +0800

    create a shimmer loading effect

---
+++ shimmer_loading_effect/lib/main.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ExampleUiLoadingAnimation(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class CircleListItem extends StatelessWidget {
+  const CircleListItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: Container(
+        width: 54,
+        height: 54,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+        child: ClipOval(
+          child: Image.network(
+            'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardListItem extends StatelessWidget {
+  final bool isLoading;
+
+  const CardListItem({
+    super.key,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildImage(),
+          const SizedBox(height: 16),
+          _buildText(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Image.network(
+            'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Food1.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    if (isLoading) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 24,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Container(
+            width: 250,
+            height: 24,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
+          'eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        ),
+      );
+    }
+  }
+}
+
+class ShimmerLoading extends StatefulWidget {
+  final bool isLoading;
+
+  final Widget child;
+
+  const ShimmerLoading({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  @override
+  State<StatefulWidget> createState() => ShimmerLoadingState();
+}
+
+const _shimmerGradient = LinearGradient(
+  colors: [
+    Color(0xFFEBEBF4),
+    Color(0xFFF4F4F4),
+    Color(0xFFEBEBF4),
+  ],
+  stops: [
+    0.1,
+    0.3,
+    0.4,
+  ],
+  begin: Alignment(-1.0, -0.3),
+  end: Alignment(1.0, 0.3),
+  tileMode: TileMode.clamp,
+);
+
+class ShimmerLoadingState extends State<ShimmerLoading> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.isLoading) {
+      return widget.child;
+    }
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) {
+        return _shimmerGradient.createShader(bounds);
+      },
+      child: widget.child,
+    );
+  }
+}
+
+class Shimmer extends StatefulWidget {
+  final LinearGradient linearGradient;
+
+  final Widget? child;
+
+  const Shimmer({
+    super.key,
+    required this.linearGradient,
+    this.child,
+  });
+
+  static ShimmerState? of(BuildContext context) {
+    return context.findAncestorStateOfType<ShimmerState>();
+  }
+
+  @override
+  State<StatefulWidget> createState() => ShimmerState();
+}
+
+class ShimmerState extends State<Shimmer> {
+  Gradient get gradient => LinearGradient(
+        colors: widget.linearGradient.colors,
+        stops: widget.linearGradient.stops,
+        begin: widget.linearGradient.begin,
+        end: widget.linearGradient.end,
+      );
+
+  bool get isSized => (context.findRenderObject() as RenderBox).hasSize;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  Offset getDescendantOffset({
+    required RenderBox descendant,
+    Offset offset = Offset.zero,
+  }) {
+    final shimmerBox = context.findRenderObject() as RenderBox;
+    return descendant.localToGlobal(offset, ancestor: shimmerBox);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child ?? const SizedBox();
+  }
+}
+
+class ExampleUiLoadingAnimation extends StatefulWidget {
+  const ExampleUiLoadingAnimation({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ExampleUiLoadingAnimationState();
+}
+
+class ExampleUiLoadingAnimationState extends State<ExampleUiLoadingAnimation> {
+  bool _isLoading = true;
+
+  void _toggleLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Shimmer(
+        linearGradient: _shimmerGradient,
+        child: ListView(
+          physics: _isLoading ? const NeverScrollableScrollPhysics() : null,
+          children: [
+            const SizedBox(height: 16),
+            _buildTopRowList(),
+            const SizedBox(height: 16),
+            _buildListItem(),
+            _buildListItem(),
+            _buildListItem(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleLoading,
+        child: Icon(_isLoading ? Icons.hourglass_full : Icons.hourglass_bottom),
+      ),
+    );
+  }
+
+  Widget _buildTopRowList() {
+    return SizedBox(
+      height: 72,
+      child: ListView(
+        physics: _isLoading ? const NeverScrollableScrollPhysics() : null,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        children: [
+          const SizedBox(width: 16),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+          _buildTopRowItem(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem() {
+    return ShimmerLoading(
+      isLoading: _isLoading,
+      child: CardListItem(
+        isLoading: _isLoading,
+      ),
+    );
+  }
+
+  Widget _buildTopRowItem() {
+    return ShimmerLoading(
+      isLoading: _isLoading,
+      child: const CircleListItem(),
+    );
+  }
+}
