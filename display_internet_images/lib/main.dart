60e02c78ecbf4719f766c04276ace1bc11978d69
wusung <twkoci@gmail.com>
2022-06-12 05:46:45 +0800

    Display images from the internet

---
+++ display_internet_images/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var title = 'Web Images';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Image.network('https://picsum.photos/300?image=9'),
+      ),
+    );
+  }
+}
