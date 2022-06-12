41341eed958c8163546e08574a2f5c932ef8704e
wusung <twkoci@gmail.com>
2022-06-12 11:17:32 +0800

    Create lists with different types of items

---
+++ different_types_lists_items/android/app/src/main/kotlin/com/example/different_types_lists_items/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.different_types_lists_items
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ different_types_lists_items/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(
+      items: List<ListItem>.generate(
+        1000,
+        (i) => i % 6 == 0
+            ? HeadingItem('Heading $i')
+            : MessageItem('Sender $i', 'Message body $i'),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final List<ListItem> items;
+
+  const MyApp({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Mixed List';
+
+    return MaterialApp(
+      title: title,
+      home: MaterialApp(
+        title: title,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(title),
+          ),
+          body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+
+              return ListTile(
+                title: item.buildTitle(context),
+                subtitle: item.buildSubtitle(context),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+abstract class ListItem {
+  Widget buildTitle(BuildContext context);
+
+  Widget buildSubtitle(BuildContext context);
+}
+
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildSubtitle(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(body);
+}
